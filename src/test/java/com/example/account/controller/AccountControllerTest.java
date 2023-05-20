808e172055ea444b7b05fd6628dc24eaa08aed88
HyunsooZo <bzhs1992@icloud.com>
2023-05-21 03:36:23 +0900

    commit

--- src/test/java/com/example/account/controller/AccountControllerTest.java
+++ src/test/java/com/example/account/controller/AccountControllerTest.java
@@ -7,7 +7,6 @@ import com.example.account.dto.DeleteAccount;
 import com.example.account.type.AccountStatus;
 import com.example.account.service.AccountService;
 import com.example.account.service.RedisTestService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
