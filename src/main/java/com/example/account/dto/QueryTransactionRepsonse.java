7c10f52aecbf033fb45025f8b168d9a4dcb61295
HyunsooZo <bzhs1992@icloud.com>
2023-05-21 21:38:01 +0900

    commit

---
+++ src/main/java/com/example/account/dto/QueryTransactionRepsonse.java
@@ -0,0 +1,31 @@
+package com.example.account.dto;
+
+import com.example.account.type.TransactionResultType;
+import com.example.account.type.TransactionType;
+import lombok.*;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class QueryTransactionRepsonse {
+    private String accountNumber;
+    private TransactionType transactionType;
+    private TransactionResultType transactionResult;
+    private String transactionId;
+    private Long amount;
+    private LocalDateTime transactedAt;
+
+    public static QueryTransactionRepsonse from(TransactionDto transactionDto) {
+        return QueryTransactionRepsonse.builder()
+                .accountNumber(transactionDto.getAccountNumber())
+                .transactionType(transactionDto.getTransactionType())
+                .transactionResult(transactionDto.getTransactionResultType())
+                .transactionId(transactionDto.getTransactionId())
+                .transactedAt(transactionDto.getTransactedAt())
+                .amount(transactionDto.getAmount())
+                .build();
+    }
+}
